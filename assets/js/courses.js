c036f333ac23c99b18810dd7d3a962f00cec3f68
DoThiThuTrang <20520816@gm.uit.edu.vn>
2023-11-20 04:01:31 +0700

    Change format

---
+++ assets/js/courses.js
@@ -0,0 +1,80 @@
+function loadHTML(url, elementId) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data;
+        });
+}
+
+function loadCSS(url) {
+    var link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    link.href = url;
+    document.head.appendChild(link);
+}
+
+function loadJS(url) {
+    var script = document.createElement("script");
+    script.src = url;
+    document.head.appendChild(script);
+}
+
+loadHTML("template/header.html", "header");
+loadHTML("template/filter.html", "filter");
+loadHTML("template/for-you-courses.html", "for-you-courses");
+loadHTML("template/all-courses.html", "all-courses");
+loadHTML("template/footer.html", "footer");
+
+loadCSS("assets/css/header.css");
+loadCSS("assets/css/filter.css");
+loadCSS("assets/css/for-you-courses.css");
+loadCSS("assets/css/all-courses.css");
+loadCSS("assets/css/footer.css");
+
+const navs = document.getElementsByClassName("nav-button");
+window.addEventListener("load", () => {
+    navs[4].style.backgroundColor = "#464bef";
+    navs[4].style.color = "white";
+})
+
+function toggleElement() {
+    var element = document.getElementById("content"); 
+    var checkbox = document.getElementById("switch-on-off"); 
+
+    if (checkbox.checked) {
+        element.style.display = "none"; 
+    } else {
+        element.style.display = "block"; 
+    }
+}
+
+function hideOtherCheckboxes(checkbox, listname) {
+    var checkboxes = document.getElementsByName(listname); 
+    for (var i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i] != checkbox) {
+            checkboxes[i].checked = false;
+        }
+    }
+}
+
+function hideListofCondition(listname) {
+    var element = document.getElementById(listname); 
+
+    if (element.style.display === "none" || element.style.display === "") {
+        element.style.display = "block"; 
+    } else {
+        element.style.display = "none"; 
+    }
+}
+
+function toggleCheckbox(checkboxId) {
+    var checkbox = document.getElementById(checkboxId);
+    if (checkbox) {
+      if (checkbox.style.display === 'none') {
+        checkbox.style.display = 'inline-block';
+      } else {
+        checkbox.style.display = 'none';
+      }
+    }
+}
