a157e607d3b162a4ccdc0a555a96096e96d3c8e2
pahopu <20520278@gm.uit.edu.vn>
2023-11-20 23:09:58 +0700

    Change unit and make modern login

--- assets/js/aboutus.js
+++ assets/js/aboutus.js
@@ -25,18 +25,67 @@ loadHTML("template/intro-aboutus.html", "intro-aboutus");
 loadHTML("template/history.html", "history");
 loadHTML("template/ourteam.html", "ourteam");
 loadHTML("template/footer.html", "footer");
+loadHTML("template/modern-login.html", "modern-login");
 
 loadCSS("assets/css/header.css");
 loadCSS("assets/css/intro-aboutus.css");
 loadCSS("assets/css/history.css");
 loadCSS("assets/css/ourteam.css");
 loadCSS("assets/css/footer.css");
+loadCSS("assets/css/modern-login.css");
+
+loadJS("assets/js/modern-login.js")
 
 const navs = document.getElementsByClassName("nav-button");
 const header = document.getElementsByClassName("header");
-const headerid = document.getElementById("header");
+const intro_aboutus = document.getElementById("intro-aboutus")
+const login = document.getElementsByClassName("log-in");
+const signup = document.getElementsByClassName("sign-up");
+
+let checkVisibility = false;
+
 window.addEventListener("load", () => {
+    const popup_container = document.querySelectorAll("#modern-login .modern-login .container");
+
     navs[4].style.backgroundColor = "#464bef";
     navs[4].style.color = "white";
-    headerid.style.marginTop = `${header[0].offsetHeight}px`;
+    intro_aboutus.style.paddingTop = `${header[0].offsetHeight}px`;
+
+
+    function blurBackground() {
+        const blur = document.getElementById("blur");
+        blur.classList.toggle('active');
+        const popup = document.getElementById("popup");
+        popup.classList.toggle('active');
+    }
+
+    login[0].addEventListener("click", () => {
+        if (!checkVisibility) {
+            blurBackground();
+            checkVisibility = true;
+            popup_container[0].classList.remove("active");
+        }
+    });
+
+    signup[0].addEventListener("click", () => {
+        if (!checkVisibility) {
+            blurBackground();
+            checkVisibility = true;
+            popup_container[0].classList.add("active");
+        }
+    });
+
+    document.addEventListener("click", (event) => {
+        const isButton = event.target.tagName === "BUTTON"; // Kiểm tra nếu là button
+        const isPopup = event.target.closest("#popup"); // Kiểm tra nếu là phần tử con của pop up
+
+        if (!isPopup && !isButton && checkVisibility) {
+            blurBackground();
+            checkVisibility = false;
+        }
+    });
 })
+
+window.addEventListener("resize", () => {
+    intro_aboutus.style.paddingTop = `${header[0].offsetHeight}px`;
+});
--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -26,6 +26,7 @@ loadHTML("template/flexer.html", "flexer");
 loadHTML("template/recommend-courses.html", "recommend-courses");
 loadHTML("template/homepage-feedback.html", "homepage-feedback");
 loadHTML("template/footer.html", "footer");
+loadHTML("template/modern-login.html", "modern-login");
 
 loadCSS("assets/css/header.css");
 loadCSS("assets/css/intro-log-in.css");
@@ -33,22 +34,54 @@ loadCSS("assets/css/flexer.css");
 loadCSS("assets/css/recommend-courses.css");
 loadCSS("assets/css/homepage-feedback.css");
 loadCSS("assets/css/footer.css");
+loadCSS("assets/css/modern-login.css");
 
 loadJS("assets/js/recommend-courses.js");
 loadJS("assets/js/homepage-feedback.js");
+loadJS("assets/js/modern-login.js")
 
 const navs = document.getElementsByClassName("nav-button");
 const signup = document.getElementsByClassName("sign-up");
 const login_intro = document.getElementsByClassName("intro-log-in");
+const login_section = document.getElementById("intro-log-in");
 const header = document.getElementsByClassName("header");
-const headerid = document.getElementById("header");
+const login = document.getElementsByClassName("log-in");
+let checkVisibility = false
 
 window.addEventListener("load", () => {
     navs[0].style.backgroundColor = "#464bef";
     navs[0].style.color = "white";
-    headerid.style.marginTop = `${header[0].offsetHeight}px`;
+    login_section.style.paddingTop = `${header[0].offsetHeight}px`;
+
+    function blurBackground() {
+        const blur = document.getElementById("blur");
+        blur.classList.toggle('active');
+        const popup = document.getElementById("popup");
+        popup.classList.toggle('active');
+    }
+
+    login[0].addEventListener("click", () => {
+        if (!checkVisibility) {
+            blurBackground();
+            checkVisibility = true;
+        }
+    });
 
     signup[0].addEventListener("click", () => {
         login_intro[0].scrollIntoView({ behavior: "smooth" });
     });
+
+    document.addEventListener("click", (event) => {
+        const isButton = event.target.tagName === "BUTTON"; // Kiểm tra nếu là button
+        const isPopup = event.target.closest("#popup"); // Kiểm tra nếu là phần tử con của pop up
+
+        if (!isPopup && !isButton && checkVisibility) {
+            blurBackground();
+            checkVisibility = false;
+        }
+    });
+});
+
+window.addEventListener("resize", () => {
+    login_section.style.paddingTop = `${header[0].offsetHeight}px`;
 });
