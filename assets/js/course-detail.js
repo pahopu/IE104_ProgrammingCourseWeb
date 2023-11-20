7c86880a19a45f9db18cfbf8b8ec558408a15a94
DoThiThuTrang <20520816@gm.uit.edu.vn>
2023-11-20 13:01:53 +0700

    Change course detail

--- assets/js/course-detail.js
+++ assets/js/course-detail.js
@@ -1,3 +1,38 @@
+function loadHTML(url, elementId) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data;
+        });
+}
+
+function loadCSS(url) {
+    var link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    link.href = url;
+    document.head.appendChild(link);
+}
+
+function loadJS(url) {
+    var script = document.createElement("script");
+    script.src = url;
+    document.head.appendChild(script);
+}
+
+loadHTML("template/header.html", "header");
+loadHTML("template/course-detail.html", "course-detail");
+loadHTML("template/footer.html", "footer");
+
+loadCSS("assets/css/header.css");
+loadCSS("assets/css/course-detail.css");
+loadCSS("assets/css/footer.css");
+
+const navs = document.getElementsByClassName("nav-button");
+window.addEventListener("load", () => {
+    navs[4].style.backgroundColor = "#464bef";
+    navs[4].style.color = "white";
+})
 function changeColor(cell) {
     var cells = document.querySelectorAll('td');
     cells.forEach(function(cell) {
