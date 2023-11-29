85d435e22f4a5344a20ba409691358cf197ec064
pahopu <20520278@gm.uit.edu.vn>
2023-11-29 16:07:05 +0700

    Add change password and update something

---
+++ assets/js/my-account-pass.js
@@ -0,0 +1,50 @@
+function loadHTML(url, elementId) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data;
+        });
+}
+
+function loadCSS(url) {
+    var link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    link.href = url;
+    document.head.appendChild(link);
+}
+
+function loadJS(url) {
+    var script = document.createElement("script");
+    script.src = url;
+    document.head.appendChild(script);
+}
+
+loadHTML("../template/header-user.html", "header-user");
+loadHTML("../template/change-password.html", "change-password")
+loadHTML("../template/footer-user.html", "footer-user");
+
+loadCSS("../assets/css/header-user.css");
+loadCSS("../assets/css/change-password.css");
+loadCSS("../assets/css/footer.css");
+
+loadJS("../assets/js/header-user.js")
+
+window.addEventListener("load", () => {
+    const navs = document.getElementsByClassName("nav-button");
+    const info = document.getElementById("change-password");
+    const header = document.getElementsByClassName("header");
+    const logout = document.getElementById("logout");
+
+    logout.href = "../homepage.html";
+
+    navs[0].style.backgroundColor = "#464bef";
+    navs[0].style.color = "white";
+    info.style.paddingTop = `${header[0].offsetHeight}px`;
+});
+
+window.addEventListener("resize", () => {
+    const info = document.getElementById("change-password");
+    const header = document.getElementsByClassName("header");
+    info.style.paddingTop = `${header[0].offsetHeight}px`;
+});
