72e4eeecb24d94be410a0417be1e57994a895623
pahopu <20520278@gm.uit.edu.vn>
2023-12-01 12:46:39 +0700

    Fix header and something else

--- assets/js/header-user.js
+++ assets/js/header-user.js
@@ -2,10 +2,22 @@ function toggleDropdown(act, id, color) {
     var dropdown = document.getElementById(id);
     var actx = document.getElementsByClassName(act)[0];
     actx.classList.toggle(color);
-    console.log(actx, color);
+
     if (dropdown.style.display == "none") {
         dropdown.style.display = "block";
     } else {
         dropdown.style.display = "none";
     }
+
+    window.onclick = function (event) {
+        if (!event.target.matches('.' + act)) {
+            console.log('yes');
+            if (dropdown.style.display == "block") {
+                dropdown.style.display = "none";
+            }
+            if (actx.classList.contains(color)) {
+                actx.classList.remove(color);
+            }
+        }
+    };
 }
--- assets/js/modern-login.js
+++ assets/js/modern-login.js
@@ -1,11 +1,13 @@
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
+window.addEventListener("load", () => {
+    const container = document.getElementById('container');
+    const registerBtn = document.getElementById('register');
+    const loginBtn = document.getElementById('login');
 
-registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
-});
+    registerBtn.addEventListener('click', () => {
+        container.classList.add("active");
+    });
 
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
-});
+    loginBtn.addEventListener('click', () => {
+        container.classList.remove("active");
+    });
+})
